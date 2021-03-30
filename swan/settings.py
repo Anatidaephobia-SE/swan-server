5a18b2f467983c6aedc1232d41aeffc0457979f9
keyvan1379 <ddssee71@gmail.com>
2021-03-30 05:56:11 -0400

    github workflow updated, django setting updated"

--- swan/settings.py
+++ swan/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'swan.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': os.getenv('POSTGRES_HOST'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': 'postgres',
