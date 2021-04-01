5ffff14c6dd09752b54a067da7b677969d1566d1
Zahra Hosseini <48201383+zahrahosseini99@users.noreply.github.com>
2021-04-01 16:03:12 +0430

    Implement post models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+def get_user_directory(instance, filename):
+    return f'uploads/'
+
+class Media(models.Model):
+    id = models.AutoField(primary_key=True)
+    media = models.FileField(upload_to=get_user_directory, null=True)
+
+class Post(models.Model):
+    name = models.CharField(blank=True, max_length=60)
+    caption = models.CharField(blank=True, max_length=280)
+    status = models.CharField(blank=True, max_length=20)
+    owner = models.ForeignKey(User, related_name='Post_owner', null=True, on_delete=models.CASCADE)
+    created_at = models.TimeField(auto_now_add=True)
+    multimedia = models.ManyToManyField(Media, related_name='Post', blank=True)
+
