33b48d6446b6045ba3b82f91681a29dc9fdc1a22
Zahra Hosseini <48201383+zahrahosseini99@users.noreply.github.com>
2021-04-01 16:37:21 +0430

    Implement create post views

--- post/views.py
+++ post/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from . import serializers as post_serializer
+from .models import Post, Media
+from users.models import User
 
-# Create your views here.
+
+class CreatePostView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    permission_classes = (IsAuthenticated,)
+    serializer_class = post_serializer.post_serializer
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        data = request.data
+        data['owner'] = user.id
+        serializer = self.get_serializer(data=data)
+        if not serializer.is_valid(True):
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        post = serializer.save()
+        for m in data['multimedia']:
+            i = Media.objects.create(media=m['media'])
+            post.multimedia.add(i)
+        return Response("OK", status=status.HTTP_202_ACCEPTED)
