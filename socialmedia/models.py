bdaf25eb2b033fbb848990d23eeb66be0dd2fef4
Hadi Sheikhi <hadsheikhi@gmail.com>
2021-04-04 01:09:47 +0430

    make relation one to one

--- socialmedia/models.py
+++ socialmedia/models.py
@@ -4,7 +4,7 @@ from team.models import Team
 
 # Create your models here.
 class SocialMedia(models.Model):
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team = models.OneToOneField(Team, on_delete=models.CASCADE)
     twitter_oauth_token = models.TextField(null=True, max_length=200)
     twitter_oauth_token_secret = models.TextField(null=True, max_length=200)
     twitter_user_id = models.TextField(null=True, max_length=200)
