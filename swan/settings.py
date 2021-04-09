3a9306e9fcced99c3a40e829eeaec6e4a5a80660
Hadi Sheikhi <hadsheikhi@gmail.com>
2021-04-09 15:01:34 +0430

    fix review notes

--- swan/settings.py
+++ swan/settings.py
@@ -107,8 +107,12 @@ WSGI_APPLICATION = 'swan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432),
     }
 }
 
