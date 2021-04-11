f5dbd2e2e571e44d0f39e0a4a6ff8d89fd93da44
keyvan1379 <ddssee71@gmail.com>
2021-04-11 05:50:14 -0400

    django url bug fixed

--- swan/urls.py
+++ swan/urls.py
@@ -25,7 +25,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("django_prometheus.urls"), name="django-prometheus"),
-    url(r'^api/users/', include(users_urls))
+    path('api/users/', include(users_urls)),
     path('api/users/', include(users_urls)),
     path('api/team/', include(team_urls))
 ]
