ee41fec0c1e9e7aab1fc6801f84645a75cb0066c
Hadi Sheikhi <hadsheikhi@gmail.com>
2021-04-13 01:53:14 +0430

    fix postgres dbname

--- swan/settings.py
+++ swan/settings.py
@@ -114,7 +114,7 @@ WSGI_APPLICATION = 'swan.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('POSTGRES_DBNAME', 'postgress'),
+        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
