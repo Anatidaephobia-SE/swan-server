27028b616cf795f9c64f007d29df8295428cc775
Zahra Hosseini <48201383+zahrahosseini99@users.noreply.github.com>
2021-05-03 02:08:30 +0430

    Override delete method

--- filestorage/serializers.py
+++ filestorage/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from . import models
+from users.models import User
+from .models import MediaStorage
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'profile_picture')
 
 class FileSerializer(serializers.ModelSerializer):
+    owner = UserSerializer(required=False)
     class Meta:
-        model = models.MediaStorage
-        fields = ('id','title','media')
+        model = MediaStorage
+        fields = ('id','title','media','team','owner')
+    read_only_fields = ('owner','team')    
+
+
