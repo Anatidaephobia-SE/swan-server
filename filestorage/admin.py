5d45d44ddf41c62e8f58a8ab2a0e589c4b629e0c
Zahra Hosseini <48201383+zahrahosseini99@users.noreply.github.com>
2021-05-02 23:03:36 +0430

    implement storage models

--- filestorage/admin.py
+++ filestorage/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import MediaStorage
 
-# Register your models here.
+admin.site.register(MediaStorage)
--- filestorage/models.py
+++ filestorage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django_minio_backend import MinioBackend, iso_date_prefix
 
-# Create your models here.
+priv_bucket = "django-backend-dev-private"
+
+class MediaStorage(models.Model):
+    title = models.CharField(max_length=255)
+    media = models.FileField(verbose_name="Object Upload",
+                            storage=MinioBackend(bucket_name=priv_bucket),
+                            upload_to=iso_date_prefix)
+
+    def __str__(self):
+        return self.title
