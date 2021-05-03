1276d7e87503d3539196add14334e03846eb85f4
Zahra Hosseini <48201383+zahrahosseini99@users.noreply.github.com>
2021-05-03 18:59:10 +0430

    Edit media url

--- filestorage/serializers.py
+++ filestorage/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from users.models import User
 from .models import MediaStorage
+from swan.settings import MINIO_ENDPOINT
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,6 +10,9 @@ class UserSerializer(serializers.ModelSerializer):
 
 class FileSerializer(serializers.ModelSerializer):
     owner = UserSerializer(required=False)
+    
+    def edit_url(self,obj):
+        return obj.media.replace(MINIO_ENDPOINT, os.getenv("BASE_URL_FOR_MINIO"))
     class Meta:
         model = MediaStorage
         fields = ('id','media','team','owner')
