865b1361726af03b90ce9ba6ce9765d93c878617
Zahra Hosseini <48201383+zahrahosseini99@users.noreply.github.com>
2021-05-18 00:56:44 +0430

    Fix blank field bugs

--- postideas/models.py
+++ postideas/models.py
@@ -7,15 +7,15 @@ class Card(models.Model):
     tag_CHOICES = (
         ('Low priority','Low priority'),
         ('High priority','High priority'),
-        ('Future works','Future works'),
+        ('Medium priority','Medium priority'),
     )
     
     title = models.CharField(null = True, max_length=60)
     description = models.CharField(blank = True, max_length=280)
     status = models.CharField(null = True, max_length=60)
-    assignee = models.ForeignKey(User, related_name = 'assignee', null=True, on_delete = models.CASCADE)
+    assignee = models.ForeignKey(User, related_name = 'assignee', blank = True, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add =True)
     team = models.ForeignKey(Team, related_name = 'workspace', null = True, on_delete = models.CASCADE)
-    tag = models.CharField(null = True, max_length=20,choices = tag_CHOICES)
+    tag = models.CharField(blank = True, max_length=20,choices = tag_CHOICES)
     
 
