fd9b8c3c8f9996cb21a9d17c9425d5d30502e222
Hadi Sheikhi <hadsheikhi@gmail.com>
2021-05-22 03:53:29 +0430

    fix undefined social media

--- post/views.py
+++ post/views.py
@@ -51,10 +51,11 @@ class CreatePostView(generics.CreateAPIView):
                 return Response("An Error has occured during publishing")
         if post.status == 'Schedule':
             sc = Scheduler()
+            socialmedia=SocialMedia.objects.all().get(team=post.team)
             if 'schedule_time' in data:
                 sc.schedule_post(post, socialmedia, TaskType.Twitter, data['schedule_time'])
                 post.schedule_time=data['schedule_time']
-                return Response(data={"message": "added to tyhe queue", "date": sc.get_post_scheduled_date(post, TaskType.Twitter)},status=status.HTTP_200_OK)
+                return Response(data={"message": "added to the queue", "date": sc.get_post_scheduled_date(post, TaskType.Twitter)},status=status.HTTP_200_OK)
         return Response(post_serializer.PostSerializer(post).data, status=status.HTTP_201_CREATED)
 
 class UpdatePostView(generics.RetrieveUpdateDestroyAPIView):
