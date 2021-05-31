d4c7ea7d638771f771b5420e8a3f912ee2c3caa1
Zahra Hosseini <48201383+zahrahosseini99@users.noreply.github.com>
2021-05-31 21:07:17 +0430

    Implement email model

---
+++ notification/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from users.models import User
+from team.models import Team
+from filestorage.models import MediaStorage
+
+class Email(models.Model):
+
+    template_name = models.CharField(null = True, max_length=60)
+    body_text = models.CharField(blank = True, max_length=700)
+    subject= models.CharField(blank = True, max_length=280)
+    recivier= models.EmailField(max_length=254)
+    sender = models.ForeignKey(Team, related_name = 'team_email', null = True, on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add =True)
+    schedule_time = models.DateTimeField(blank=True, null=True)
