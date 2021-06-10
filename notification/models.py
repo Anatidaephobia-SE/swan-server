9cdd5b201d144cc0d459ffe1a0d551dd7b817dcd
Zahra Hosseini <48201383+zahrahosseini99@users.noreply.github.com>
2021-06-10 18:26:34 +0430

    Edit model fields

--- notification/models.py
+++ notification/models.py
@@ -6,7 +6,7 @@ class Email(models.Model):
     template_name = models.CharField(null = True, max_length=60)
     body_text = models.CharField(blank = True, max_length=700)
     subject= models.CharField(blank = True, max_length=280)
-    recivier= models.EmailField(max_length=254)
+    reciviers = models.TextField(null=False, max_length=1000)
     sender = models.ForeignKey(Team, related_name = 'team_email', null = True, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add =True)
     schedule_time = models.DateTimeField(blank=True, null=True)
