f3e28c36565378b6cfb4fcb1ecad39f7019d8071
Zahra Hosseini <48201383+zahrahosseini99@users.noreply.github.com>
2021-06-10 21:02:51 +0430

    Add status field

--- notification/views.py
+++ notification/views.py
@@ -1,18 +1,12 @@
 from rest_framework import generics
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from . import serializers as template_serializer
 from .models import Template
-from users.models import User
 from team.models import Team
-from socialmedia.models import SocialMedia
-from socialmedia.twitter import Tweet
-from scheduler.scheduler import Scheduler
-from scheduler.models import TaskType
-from datetime import datetime
-from filestorage.models import MediaStorage
 from .reciever_retrieval import recieve_mail_list
+
 class CreateTemplatetView(generics.CreateAPIView):
     queryset = Template.objects.all()
     permission_classes = (IsAuthenticated,)
@@ -28,31 +22,16 @@ class CreateTemplatetView(generics.CreateAPIView):
         reciviers = data['reciviers']
         sender=data['sender']
         template_team=Team.objects.get(pk=data['template_team'])
-        status=data['status']
-        temp=Template.objects.create(name=template_name,reciviers=reciviers,sender=sender,template_team=template_team,status=status)
+        template_status=data['status']
+        temp=Template.objects.create(name=template_name,reciviers=reciviers,sender=sender,template_team=template_team,status=template_status)
 
         if 'body_text' in data:
             temp.body_text = data['body_text']
         if 'subject' in data:
             temp.subject = data['subject']
-        emails=recieve_mail_list(reciviers)
-        print(emails)
-        #for e in emails:
-            #send_email()
-        # if post.status == 'Published':
-        #     socialmedia=SocialMedia.objects.all().get(team=post.team)
-        #     twitter_response, published_id = Tweet(post,socialmedia)
-        #     post.published_id=published_id
-        #     post.schedule_time = datetime.now()
-        #     post.save()
-        #     if twitter_response.status_code != 200 :
-        #         return Response("An Error has occured during publishing")
-        # if post.status == 'Schedule':
-        #     sc = Scheduler()
-        #     socialmedia=SocialMedia.objects.all().get(team=post.team)
-        #     if 'schedule_time' in data:
-        #         sc.schedule_post(post, socialmedia, TaskType.Twitter, data['schedule_time'])
-        #         post.schedule_time=data['schedule_time']
-        #         post.save()
-        #         return Response(data={"message": "added to the queue", "date": sc.get_post_scheduled_date(post, TaskType.Twitter)},status=status.HTTP_200_OK)
+        if temp.status=='Send':
+            emails=recieve_mail_list(reciviers)
+            print(emails)
+            #for e in emails:
+                #send_email(temp_subject,body_text,...)
         return Response(template_serializer.TemplateSerializer(temp).data, status=status.HTTP_201_CREATED)
