d1f4d1a5b809095fcc0284d4afc62f001e95ea4f
Zahra Hosseini <48201383+zahrahosseini99@users.noreply.github.com>
2021-06-10 18:44:45 +0430

    Edit model name

--- notification/serializers.py
+++ notification/serializers.py
@@ -3,6 +3,6 @@ from . import models
 
 class TemplateSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Email
-        fields = ('id','template_name', 'body_text', 'subject', 'reciviers','sender','created_at','schedule_time')
+        model = models.Template
+        fields = ('id','name', 'body_text', 'subject', 'reciviers','sender','template_team','created_at','schedule_time')
     read_only_fields = ('id')
